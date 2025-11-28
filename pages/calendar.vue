6c4661a4c8618a3d1936d42915807f57aecdd380
Louis-Nguyen27 <louisnguyen@talentgetgo.com>
2025-11-28 14:25:16 +0700

    date input

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 const d = new Date();
+const inputDate = ref();
 </script>
 <template>
+    <input type="date" v-model="inputDate" />
+    <button>
+        {{ inputDate }}
+    </button>
+    <div>Date string: {{ inputDate.toLocaleString() }}</div>
     <div>Locale string: {{ d.toLocaleString() }}</div>
     <div>UTC: {{ d.toUTCString() }}</div>
     <WeekCalendar />

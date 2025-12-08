9852b055a22ded993f4d58d8669153e223ce427a
Louis-Nguyen27 <louisnguyen@talentgetgo.com>
2025-12-08 10:47:59 +0700

    check location

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 const d = new Date();
 const inputDate = ref();
+const location = ref("");
+onBeforeMount(() => {
+    location.value = window.location.href;
+});
 </script>
 <template>
     <input type="date" v-model="inputDate" />
     <button>
         {{ inputDate }}
     </button>
-    <div>Date string: {{ inputDate.toLocaleString() }}</div>
+    <!-- <div>Date string: {{ inputDate.toLocaleString() }}</div> -->
+    <div>{{ location }}</div>
     <div>Locale string: {{ d.toLocaleString() }}</div>
     <div>UTC: {{ d.toUTCString() }}</div>
     <WeekCalendar />
